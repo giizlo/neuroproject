cacdef1e2cf5947089e2f17bbdfa733e16668edf
giizlo <g.satushev@gmail.com>
2024-11-26 04:32:31 +0500

    serv changes

--- src/main/start.js
+++ src/main/start.js
@@ -1,16 +1,40 @@
 const http = require("node:http");
 const fs = require("node:fs");
 
-const settings = 'properties.json';
-const props = JSON.parse(fs.readFileSync(settings, 'utf8'))
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.statusMessage = "test bitch";
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("You are first");
-});
 
-server.listen(props.port, props.host, () => {
-    console.log(`//server is listening, running https://${props.host}:${props.port}/`);
-});
+class serv {
+    constructor(settings) {
+        this.props = JSON.parse(fs.readFileSync(settings, 'utf8'));
+    }
+
+    run() {
+        this.server = http.createServer((req, res) => {
+            res.end("You are first");
+        });
+        
+        this.server.listen(this.props.port, this.props.host, () => {
+            console.log(`//server is listening, running https://${this.props.host}:${this.props.port}/`);
+            // this.update();
+        });
+    }
+
+    update() {
+        this.running = 1;
+        // while (this.running) {
+        // }
+        console.log(this.server.listeners);
+    }
+
+    OnDataReceived() {}
+
+    auth() {}
+
+    client() {}
+
+    broadcast() {}
+    
+}
+
+const Main = new serv('properties.json');
+Main.run();
