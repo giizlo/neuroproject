a8804095912259088d8f941de3bbf07663f06e05
giizlo <g.satushev@gmail.com>
2024-11-26 06:17:24 +0500

    serv upd

--- src/main/start.js
+++ src/main/start.js
@@ -6,16 +6,25 @@ const fs = require("node:fs");
 class serv {
     constructor(settings) {
         this.props = JSON.parse(fs.readFileSync(settings, 'utf8'));
+        this.run();        
     }
 
     run() {
         this.server = http.createServer((req, res) => {
-            res.end("You are first");
+            // console.log(req.client)
+            this.update();
+            if (/\/auth.*/.test(req.url)){
+                this.auth(req);
+                res.end("Auth page");
+            }
+            else {
+                res.end("send ");
+            }
         });
         
         this.server.listen(this.props.port, this.props.host, () => {
             console.log(`//server is listening, running https://${this.props.host}:${this.props.port}/`);
-            // this.update();
+            this.update();
         });
     }
 
@@ -23,12 +32,24 @@ class serv {
         this.running = 1;
         // while (this.running) {
         // }
-        console.log(this.server.listeners);
+        for (let i in this.server._connections) {
+            console.log(1, i);
+        }
     }
 
     OnDataReceived() {}
 
-    auth() {}
+    auth(req) {
+        console.log(`STATUS: ${req.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(req.headers)}`);
+        req.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`, );
+        });
+        
+        req.on('end', () => {
+            console.log('No more data in response.')
+        });
+    }
 
     client() {}
 
@@ -37,4 +58,3 @@ class serv {
 }
 
 const Main = new serv('properties.json');
-Main.run();
